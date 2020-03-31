423cc00474debf44c8de44b5950c8e168e3c57d9
cocole <2377255ss@163.com>
2020-03-31 23:31:59 +0800

    no message

---
+++ SSPanel-Uim/src/Services/Mail/SendGrid.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services\Mail;
+
+use App\Services\Config;
+
+class SendGrid extends Base
+{
+    private $config;
+    private $sg;
+    private $sender;
+    private $name;
+
+    public function __construct()
+    {
+        $this->config = $this->getConfig();
+        $this->sg = new \SendGrid($this->config['key']);
+        $this->sender = $this->config['sender'];
+        $this->name = $this->config['name'];
+        $this->email = new \SendGrid\Mail\Mail();
+    }
+
+    public function getConfig()
+    {
+        return [
+            'key' => Config::get('sendgrid_key'),
+            'sender' => Config::get('sendgrid_sender'),
+            'name' => Config::get('sendgrid_name')
+        ];
+    }
+
+    public function send($to_address, $subject_raw, $text, $files)
+    {
+        $this->email->setFrom($this->sender, $this->name);
+        $this->email->setSubject($subject_raw);
+        $this->email->addTo($to_address,null);
+        $this->email->addContent('text/html', $text);	
+		
+        foreach ($files as $file) {
+            $this->email->addAttachment(
+                base64_encode(file_get_contents($file)),
+                'application/octet-stream',
+                basename($file),
+                'attachment',
+                'backup'
+            );
+        }
+
+        $response = $this->sg->send($this->email);
+        echo $response->body();
+    }
+}
